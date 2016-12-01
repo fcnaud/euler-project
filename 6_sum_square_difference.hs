8739ebbf21571779c14be1ac12d565cca4c029ed
dcfxyz <dcf_xyz@outlook.com>
2016-12-01 21:54:32 +0800

    modify

--- 6_sum_square_difference.hs
+++ 6_sum_square_difference.hs
@@ -1,5 +1,18 @@
+import Control.Monad
+
+solve :: Int -> Int
+solve n = a*a - b
+  where a = sum [1..n]
+        b = sum $ map (\x -> x*x) [1..n]
+
+ret :: Int
+ret = solve 100
+
 main = do
-  print (b*b-a)
-  where 
-    b = sum [1..100]
-    a = sum $ map (\x -> x*x) [1..100]
+  n <- readLn :: IO Int
+  ansList <- replicateM n $ do
+    num <- readLn :: IO Int
+    return (solve num)
+    
+  mapM_ (putStrLn.show) ansList
+
